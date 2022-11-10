37a657a41212afea7f03b30d2be10c3f1fe351ac
Kacha <jorapijo42@gmail.com>
2022-11-10 08:42:30 +0100

    conversacion entre nosotros

--- src/socketBasico/Cliente.java
+++ src/socketBasico/Cliente.java
@@ -10,7 +10,9 @@ public class Cliente {
     Socket cliente;
     String message;
     DataOutputStream output;
-
+    DataInputStream input;
+    String msgReceived;
+    boolean chat = true;
     public static void main(String[] args) throws IOException {
         Cliente client = new Cliente();
         client.initClient();
@@ -18,14 +20,29 @@ public class Cliente {
 
     //initialize the client
     public void initClient() throws IOException {
-        cliente = new Socket("192.168.1.219", PORT);
-        System.out.println("Write message...");
-        Scanner sc = new Scanner(System.in);
-        message = sc.nextLine();
-        output = new DataOutputStream(cliente.getOutputStream());
-        output.writeUTF(message);
-        System.out.println("Message send");
+        cliente = new Socket("Localhost", PORT);
+
+        do{
+            System.out.println("Write message...");
+            Scanner sc = new Scanner(System.in);
+            message = sc.nextLine();
+            output = new DataOutputStream(cliente.getOutputStream());
+            output.writeUTF(message);
+            System.out.println("Message send");
+
+            input = new DataInputStream(cliente.getInputStream());
+            msgReceived = input.readUTF();
+
+            if(msgReceived.equals("N")){
+                System.out.println("Se ha cerrado la conversación");
+                chat = false;
+            }else{
+                System.out.println("\tMessage: " +  msgReceived);
+            }
+        }while (chat);
+
         output.close();
+        input.close();
         cliente.close();
     }
 }
--- src/socketBasico/Servidor.java
+++ src/socketBasico/Servidor.java
@@ -1,9 +1,11 @@
 package socketBasico;
 
 import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Servidor {
 
@@ -12,6 +14,9 @@ public class Servidor {
     Socket socket;
     DataInputStream input;
     String msgReceived;
+    String msgSend;
+    DataOutputStream output;
+    boolean chat = true;
 
     public static void main(String[] args) throws IOException {
         Servidor server = new Servidor();
@@ -22,16 +27,35 @@ public class Servidor {
         server = new ServerSocket(PORT);
         socket = new Socket();
 
+        // espera la conexión del cliente
         System.out.println("Waiting Connection...");
         socket = server.accept();
         input = new DataInputStream(socket.getInputStream());
         System.out.println("Client connected");
-        msgReceived = input.readUTF();
-        System.out.println("Message: " + msgReceived);
 
+
+        do{
+            msgReceived = input.readUTF();
+            // saca en pantalla el mensaje enviado por el cliente
+            System.out.println("\tMessage: " + msgReceived);
+
+            // escribe el mensaje que le mandará al cliente
+            System.out.println("Write message...");
+            Scanner sc = new Scanner(System.in);
+            msgSend = sc.nextLine();
+            output = new DataOutputStream(socket.getOutputStream());
+            output.writeUTF(msgSend);
+            System.out.println("Message send");
+            if(msgSend.equals("N")){
+                chat = false;
+            }
+        }while(chat);
+
+
+        output.close();
         input.close();
         socket.close();
         server.close();
-        System.out.println("End Connecion");
+        System.out.println("End Connection");
     }
 }
